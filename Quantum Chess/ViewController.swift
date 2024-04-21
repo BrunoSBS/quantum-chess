858c65d299c90dadabb8357466b5da2f84d7bd30
Bruno <bbarton_singer@me.com>
2024-04-21 18:42:00 +0300

    Initial Commit

---
+++ Quantum Chess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quantum Chess
+//
+//  Created by Bruno Barton-Singer on 21/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
