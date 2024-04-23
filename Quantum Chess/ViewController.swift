6590feaf161ffa9b22bdf4e08620462a1f4715a6
Bruno <bbarton_singer@me.com>
2024-04-23 22:37:49 +0300

    Lessons 6-8

--- Quantum Chess/ViewController.swift
+++ Quantum Chess/ViewController.swift
@@ -1,16 +1,17 @@
 //
 //  ViewController.swift
-//  Quantum Chess
+//  Chess
 //
 //  Created by Bruno Barton-Singer on 21/04/2024.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ChessDelegate {
+
+    
 
     var chessEngine: ChessEngine = ChessEngine()
-    
     
     @IBOutlet weak var boardView: BoardView!
     override func viewDidLoad() {
@@ -18,12 +19,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         chessEngine.initializeGame()
         boardView.pieces = chessEngine.pieces
-        //boardView.setNeedsDisplay()
+        boardView.setNeedsDisplay()
+        
+        boardView.chessDelegate = self
+        
+    }
+    
+    // move piece using chessEngine, then update pieces and redraw screen
+    func movePiece(fromCol: Int, fromRow: Int, toCol: Int, toRow: Int){
+        chessEngine.movePiece(fromCol: fromCol, fromRow: fromRow, toCol: toCol, toRow: toRow)
+        boardView.pieces = chessEngine.pieces
+        boardView.setNeedsDisplay()
+    }
+    
+    func pieceAt(col: Int, row: Int) -> ChessPiece? {
+        chessEngine.pieceAt(col: col, row: row)
     }
 
     
 
 }
-
-
 
